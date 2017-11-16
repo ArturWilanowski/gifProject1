1d52943d695d6dfb4c38da9a9c97a7307fffc37f
Artur Wilanowski <arturwilanowski@gmail.com>
2017-11-16 16:59:50 +0100

    P1ZAD1 NHGHGH

---
+++ src/main/java/pl/wilanowskiperek/gifProject/GifProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.wilanowskiperek.gifProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifProjectApplication.class, args);
+	}
+}

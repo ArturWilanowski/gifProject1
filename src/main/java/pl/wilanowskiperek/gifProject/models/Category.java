d45b2fff196a7cfee41d4b9b405e997d706f942a
Perek Michal <perekmichal93@gmail.com>
2017-11-17 12:06:55 +0100

    P2ZAD3 probably working good

---
+++ src/main/java/pl/wilanowskiperek/gifProject/models/Category.java
@@ -0,0 +1,75 @@
+package pl.wilanowskiperek.gifProject.models;
+
+import pl.wilanowskiperek.gifProject.models.Gif;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Category {
+
+    private Integer id;
+    private String name;
+    private List<Gif> gifs = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public List<Gif> getGifs() {
+        return gifs;
+    }
+
+    public void setGifs(List<Gif> gifs) {
+        this.gifs = gifs;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addGifsToCattegory(Gif gif) {
+        gifs.add(gif);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) &&
+                Objects.equals(name, category.name) &&
+                Objects.equals(gifs, category.gifs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, gifs);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gifs=" + gifs +
+                '}';
+    }
+}
---
+++ src/main/java/pl/wilanowskiperek/gifProject/repository/CategoryRepository.java
@@ -0,0 +1,52 @@
+package pl.wilanowskiperek.gifProject.repository;
+
+import org.springframework.stereotype.Repository;
+import pl.wilanowskiperek.gifProject.models.Category;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class CategoryRepository {
+
+    private List<Category> categoryList = new ArrayList<>();
+
+    public void add(Category category) {
+        categoryList.add(category);
+    }
+
+    public List<Category> findAll() {
+        return categoryList;
+    }
+
+    public Category getCategory(int id) {
+        for (Category category : categoryList) {
+            if (category.getId().equals(id)) {
+                return category;
+            }
+        }
+        return new Category();
+    }
+
+    public Category find(int id) {
+        for (Category category : categoryList) {
+            if (category.getId().equals(id)) {
+                return category;
+            }
+        }
+        return new Category(); //not found ??
+    }
+
+    public Category findByName(String name) {
+        for (Category category : categoryList) {
+            if (category.getName().equals(name)) {
+                return category;
+            }
+        }
+        return new Category(); //not found ??
+    }
+}
+
+
